f8e402612306be692d7198cd2f64c4b4d13a3f25
Mehmet Celal Kara <info@mcelal.com>
2023-03-04 17:04:46 +0300

    Edit contacts list page

--- resources/views/contact/list.blade.php
+++ resources/views/contact/list.blade.php
@@ -31,10 +31,21 @@
                                     <tbody>
                                     @foreach($contacts as $contact)
                                         <tr class="border-b transition duration-300 ease-in-out hover:bg-neutral-100 dark:border-neutral-500 dark:hover:bg-neutral-600">
-                                            <td class="whitespace-nowrap px-6 py-4">{{ $contact->name }}</td>
+                                            <td class="whitespace-nowrap px-6 py-4 flex items-center gap-x-1">
+                                                <img class="object-cover w-8 h-8 rounded-full" src="{{ asset($contact->photo ?? 'uploads/profile-blank.webp') }}" alt="">
+                                                {{ $contact->name }}
+                                            </td>
                                             <td class="whitespace-nowrap px-6 py-4">{{ $contact->last_name }}</td>
                                             <td class="whitespace-nowrap px-6 py-4">{{ $contact->company }}</td>
-                                            <td class="whitespace-nowrap px-6 py-4">
+                                            <td>
+                                                <div class="flex items-center gap-x-2">
+                                                    <div>
+                                                        <h2 class="text-sm font-medium text-gray-800 dark:text-white ">Andi Lane</h2>
+                                                        <p class="text-xs font-normal text-gray-600 dark:text-gray-400">andi@example.com</p>
+                                                    </div>
+                                                </div>
+                                            </td>
+                                            <td class="whitespace-nowrap px-6 py-4 flex gap-x-0.5">
                                                 <a
                                                     class="text-blue-500 transition-colors duration-200 hover:text-indigo-500 focus:outline-none"
                                                     href="{{ route('contact.edit', [$contact]) }}"
@@ -42,12 +53,13 @@
                                                     Edit
                                                 </a>
                                                 |
-                                                <a
-                                                    class="text-blue-500 transition-colors duration-200 hover:text-indigo-500 focus:outline-none"
-                                                    href="{{ route('contact.destroy', [$contact]) }}"
-                                                >
-                                                    Delete
-                                                </a>
+                                                <form action="{{ route('contact.destroy', [$contact]) }}" method="post" onsubmit="return confirm('Are you sure to delete?')">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <button type="submit" class="text-blue-500 transition-colors duration-200 hover:text-indigo-500 focus:outline-none">
+                                                        Delete
+                                                    </button>
+                                                </form>
                                             </td>
                                         </tr>
                                     @endforeach
